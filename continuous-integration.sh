1e05c540c9a8732134035111f1d7da0ad646ad3a
Ulises Santana <ulisesantana@gmail.com>
2021-02-27 14:23:21 +0000

    💚 fix CI script

--- continuous-integration.sh
+++ continuous-integration.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function update() {
-    git pull
+  git pull
 
   VERSION=$(node -pe "require('./package.json').name.concat(':').concat(require('./package.json').version)")
   echo $VERSION
 
   npm run docker:generate
   docker rm -f smarthome
-  docker run -d --name smarthome -e PORT=3010 -e LIFX_TOKEN=$LIFX_TOKEN --network host --restart=unless-stopped smarthome-api:$VERSION
+  docker run -d --name smarthome -e PORT=3010 -e LIFX_TOKEN=$LIFX_TOKEN --network host --restart=unless-stopped $VERSION
 }
 
-update &> update-output.txt
+update &>update-output.txt
