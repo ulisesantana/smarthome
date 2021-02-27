3cf8aa5f6055f09c496d26c3f7e32fab93978051
Ulises Santana <ulisesantana@gmail.com>
2021-02-27 19:30:42 +0000

    🔖 v2.3.0

--- continuous-integration.sh
+++ continuous-integration.sh
@@ -10,11 +10,10 @@ function update() {
   VERSION="$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')"
   DOCKER_IMAGE=$PACKAGE_NAME:$VERSION
   echo $DOCKER_IMAGE
-  echo $LIFX_TOKEN
 
   docker build -t $DOCKER_IMAGE .
   docker rm -f smarthome
-  docker run -d --name smarthome -e PORT=3010 -e LIFX_TOKEN=$($LIFX_TOKEN) --network host --restart=unless-stopped $DOCKER_IMAGE
+  docker run -d --name smarthome -e PORT=3010 -e LIFX_TOKEN="$($LIFX_TOKEN)" --network host --restart=unless-stopped $DOCKER_IMAGE
 }
 
 update &>update-output.txt
