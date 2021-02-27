55fe3d84dd9335fd1b50083e0eafbf97df862161
Ulises Santana <ulisesantana@gmail.com>
2021-02-27 13:58:21 +0000

    ⚗️ testing stuff for CI

--- continuous-integration.sh
+++ continuous-integration.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-git pull
+function update() {
+    git pull
 
-VERSION=$(node -pe "require('./package.json').name.concat(':').concat(require('./package.json').version)")
-echo $VERSION
+  VERSION=$(node -pe "require('./package.json').name.concat(':').concat(require('./package.json').version)")
+  echo $VERSION
 
-npm run docker:generate
-docker rm -f smarthome
-docker run -d --name smarthome -e PORT=3010 -e LIFX_TOKEN=$LIFX_TOKEN --network host --restart=unless-stopped smarthome-api:$VERSION
+  npm run docker:generate
+  docker rm -f smarthome
+  docker run -d --name smarthome -e PORT=3010 -e LIFX_TOKEN=$LIFX_TOKEN --network host --restart=unless-stopped smarthome-api:$VERSION
+}
+
+update > update-output.txt
