8e48ccae54c2a40ac12ac460b58fd7809d54839e
Ulises Santana <ulisesantana@gmail.com>
2021-04-04 13:16:41 +0100

    🔨 update swagger docs

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,12 +1,30 @@
+import { version } from '../../package.json'
+
+interface SwaggerConfig {
+    routePrefix: string,
+    host: string,
+    exposeRoute: boolean,
+    swagger: {
+        produces: string[],
+        schemes: string[],
+        info: {
+            description: string,
+            title: string,
+            version: string
+        },
+        consumes: string[]
+    }
+}
+
 export const generateSwaggerConfig = (
   docsHost = '0.0.0.0:3000',
   docsRoute = '/docs'
-): { routePrefix: string, host: string, exposeRoute: boolean, swagger: { produces: string[], schemes: string[], info: { description: string, title: string, version: string }, consumes: string[] } } => ({
+): SwaggerConfig => ({
   swagger: {
     info: {
       title: 'Smart home API',
       description: 'API Rest for my home lights.',
-      version: '0.1.0'
+      version
     },
     schemes: ['http'],
     consumes: ['application/json'],
